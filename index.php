7ea0d9121234399e11fcae4263ba11b773ec0c70
DanB-66 <sugarlumpltd@gmail.com>
2014-07-30 19:28:59 +0100

    post rework of wrappers

--- index.php
+++ index.php
@@ -15,8 +15,8 @@
 </head>
 <body>
 
-<div id="pageWrapper">
-	<div id="pageContent">
+
+<div id="pageContent">
 
 <?php include("includes/_header_global.php"); ?>
 
@@ -26,12 +26,12 @@
 			...and it's early stages. Still reading? It uses *json/cws_en.json*  content files, *js/main.js* (via a require.js *js/app.js* stub) to render that out into a carousel/paging system via *templates/_cws.tmpl.js* dust templates, which are very simple so far but do demo conditional logic eg absent nodes in the data. *sass/screen_v42.scss* Sass helps (a lot...) with the styling. Native browser back/fwd will drive the UI and state is maintained cross-browser eg bookmarking retaining 'page', 'multiple' and 'lang' selections. Works in IE8. 3D transitions: just a start... There will be some sort of *projects.php* non js content rendered by php from the same json. Completely incomplete!
 -->
 
-		<div class="carouselWrap">
-			<a href="projects.php">sitemap...</a>
-		</div>
+	<div class="carouselWrap">
+		<a href="projects.php">sitemap...</a>
+	</div>
 
-	</div><!-- /#pageContent -->
-</div><!-- /#pageWrapper -->
+</div><!-- /#pageContent -->
+
 
 <?php include("includes/_footer_global.php"); ?>
 
