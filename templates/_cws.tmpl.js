946c137b4dad4658fdcc245759e80d63ecc992ff
DanB-66 <sugarlumpltd@gmail.com>
2014-07-30 18:48:47 +0100

    scrollbars in place etc prior to rework wrappers

--- templates/_cws.tmpl.js
+++ templates/_cws.tmpl.js
@@ -2,64 +2,69 @@
 
 <article>
 
-    <div class="articleInner" style="background-image:url(
-    {?assets.images}
-        {#assets.images[0]}{!ie only first image for bg!}
-            {.url}
-        {/assets.images[0]}
-    {/assets.images}
-    )">
+    <div class="articleInner">
+
+        <div class="hero">
+            <img src="
+                {?assets.images}
+                    {#assets.images[0]}{!ie only first image for bg!}
+                        {.url}
+                    {/assets.images[0]}
+                {/assets.images}
+            "/>
+        </div>
 
         <h3>{projectName}</h3>
-        <dl>
-            <dt>{uIclient}:</dt>
-            <dd>{client}</dd>
 
+        <section class="projectDescription">
+            <p>{projectDescription}:</p>
+        </section>
+
+        <dl class="client">
             <dt>{uIagency}:</dt>
             <dd>{agency}</dd>
-
+            <dt>{uIclient}:</dt>
+            <dd>{client}</dd>
         </dl>
-        <dl>
 
-            <dt>{uIprojectDescription}:</dt>
-            <dd>{projectDescription}</dd>
-
-            <dt>{uIworkUndertaken}:</dt>
-            <dd>{extendedInfo.workUndertaken|s}</dd>
-
+        <section class="jobDetail">
+            <h4>{uIworkUndertaken}:</h4>
+            <p>{extendedInfo.workUndertaken|s}</p>
             {?extendedInfo.otherDetails}
-                <dt>{uIotherDetails}:</dt>
-                <dd>
-                    <ol>
+                <div class="otherDetails">
+                    <h5>{uIotherDetails}:</h5>
+                    <ul class="otherDetailsList">
                         {#extendedInfo.otherDetails}
                             <li>{.}</li>
-                        {/extendedInfo.otherDetails}
-                    </ol>
-                </dd>
+                        {/extendedInfo.otherDetails}               
+                    </ul>
+                </div>
             {/extendedInfo.otherDetails}
+        </section>
 
-            {?assets.images}
-                <dt>{uIimages}:</dt>
-                <dd>
-                    <ul>
-                        {#assets.images}
-                            <li><img src="{.url}" alt="{.title}"/></li>
-                        {/assets.images}
-                    </ul>
-                </dd>
-            {/assets.images}
-            
-            {?assets.links}
-                <dt>{uIlinks}:</dt>
-                <dd>
-                    <ul>
-                        {#assets.links}
-                            <li><a href="{.url}" title="{.title}">{.title}</a></li>
-                        {/assets.links}
-                    </ul>
-                </dd>
-            {/assets.links}
-        </dl>
+        {?assets.images}
+            <section class="projImages">
+                <h5>{uIimages}:</h5>
+                <ul>
+                    {#assets.images}
+                        <li><img src="{.url}" alt="{.title}"/></li>
+                    {/assets.images}
+                </ul>
+            </section>
+        {/assets.images}
+
+
+        {?assets.links}
+            <section class="projLinks">
+                <h5>{uIlinks}:</h5>
+                <ul>
+                    {#assets.links}
+                        <li><a href="{.url}" title="{.title}">{.title}</a></li>
+                    {/assets.links}
+                </ul>
+            </section>
+        {/assets.links}
+
 
     </div>
 
